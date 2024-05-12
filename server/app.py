6e302b2af097586f2ce4b9da759e774f4d7ee44b
Khalid Sheikh Abdirahman <mac@KHALID.local>
2024-05-12 19:20:42 +0300

    feat: Extract email and password from request JSON data

--- server/app.py
+++ server/app.py
@@ -2,8 +2,9 @@
 
 import os
 from datetime import datetime
-from flask import request, jsonify, make_response, session
+from flask import request, jsonify, make_response
 from flask_restful import Api, Resource
+from flask_jwt_extended import jwt_required, create_access_token
 from sqlalchemy.exc import IntegrityError
 from config import app, db, api  
 from models import User, DonationRequest, Donation, Category 
@@ -12,7 +13,15 @@ from models import User, DonationRequest, Donation, Category
 @app.route('/')
 def index():
     return '<h1>Project Server</h1>'
+#aunthenticate a user
+
+class LoginResource(Resource):
+    def post(self):
+        email = request.json.get('email', None)
+        password = request.json.get('password', None)
+
 
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+    
